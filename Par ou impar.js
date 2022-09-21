8beb9d36d951467fdeefa7941c5280eb767da441
camilalops <111904477+camilalops@users.noreply.github.com>
2022-09-21 14:47:48 -0300

    Add files via upload

---
+++ Par ou impar.js
@@ -0,0 +1,21 @@
+programa
+
+	
+	funcao inicio()
+	{
+		//ler um número e dizer se é par ou ímpar
+		inteiro num
+		escreva("Digite um número inteiro:")
+		leia(num)
+
+		se(num % 2 == 1){
+			escreva("Ímpar...\n")
+			
+		}
+		senao{
+			escreva("Par...\n")
+			
+		}
+		
+	}
+}
